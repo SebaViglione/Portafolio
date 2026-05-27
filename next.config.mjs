9bc9416a8aa747067cbc31254e34d69ac5387ea9
SebaViglione <sebaviglione10@gmail.com>
2026-05-27 10:46:14 -0300

    fix: export static site for netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/index.html',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/gracias.html',
-        destination: '/gracias',
-        permanent: true,
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
